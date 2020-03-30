cc20520d96a4d27011240faf58789a189e80d416
Steven Malmgren <4smalmgren@gmail.com>
2020-03-29 17:48:30 -0700

    Delete config.js

--- web-led/src/config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    serverIp: '72.217.68.14',
-    localStreamAddress: '192.168.0.61',
-    localGridAddress: '192.168.0.228',
-    streamPort: '25573',
-    gridPort: '25566',
-}
